1df2b38dd248c1652f8f8abe68d0b5ce9aa6d6dd
hudamerchant <hudazehra2510@gmail.com>
2021-04-20 02:59:24 +0500

    custom styles added

--- wp-content/themes/divi_child/functions.php
+++ wp-content/themes/divi_child/functions.php
@@ -6,15 +6,17 @@ function my_theme_enqueue_styles() {
     $theme = wp_get_theme();
     
     wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css',array() , $theme->parent()->get('Version'));
-    wp_enqueue_style( 'child-style', get_stylesheet_uri().'?v=1',array( $parenthandle ) ,$theme->get('Version'));
+    wp_enqueue_style( 'child-style', get_stylesheet_uri().'',array( $parenthandle ) ,$theme->get('Version'));
     wp_enqueue_style( 'slick', get_stylesheet_directory_uri(). '/css/slick.css');
     wp_enqueue_style( 'slick_theme', get_stylesheet_directory_uri(). '/css/slick-theme.css');
+    // wp_enqueue_style( 'font-awesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
 
 
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js');
     wp_enqueue_script('slick-js', get_stylesheet_directory_uri().'/js/slick.min.js');
     wp_enqueue_script('child-script', get_stylesheet_directory_uri().'/js/script.js?v=1', 
-    ['jquery', 'slick-js']
-);
+    ['jquery', 'slick-js']);
+
+    wp_enqueue_script('font-awesome', "https://kit.fontawesome.com/c007291ff9.js");
 
 }
